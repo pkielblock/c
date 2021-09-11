31aebe9f060198717bbb5e84b3ffa39924819423
Pedro Kielblock <p.kielblock123@gmail.com>
2021-09-10 21:47:59 -0300

    Array File

--- 09-Array.c
+++ 09-Array.c
@@ -1,9 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int array[20], i;
+    double notas[4];
+    int i;
 
+    for (i = 0; i <= 3; i++)
+    {
+        printf("Digite a Nota: ");
+        scanf("%lf", &notas[i]);
+    }
 
+    for (i = 0; i <= 3; i++)
+    {
+        printf("Valor da Nota[%d]: %.1lf\n", i, notas[i]);
+    }
 
     return 0;
 }
