ccbceae81f14de41280495fc92adcf55bb6c6f73
Pedro Kielblock <p.kielblock123@gmail.com>
2022-02-21 15:03:26 -0300

    Ponteiro Vetor

---
+++ Estrutura de Dados/ponteiroVetor.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	int vet[4]={1,2,3,4};
+	int i;
+	printf("Endereço do &vet=%u\n",vet);
+	int *pv;//ponteiro do tipo inteiro
+
+	pv=vet; //pv=&vet[0]
+	printf("Endereco contido em pv=%u\n",pv);
+	printf("Vetor\n");
+	for(i=0; i<4; i++, pv++)
+	   printf("%d - %d\n",*pv, pv);
+	printf("\n");
+	/*
+	for(i=0;i<4;i++)
+	   printf("%d - %d\n",pv[i], &pv[i]);
+	printf("\n");
+	*/
+	printf("Endereco contido em pv=%u\n",pv);
+}
